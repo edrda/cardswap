1c80251d8dcbc5b91db93af0dc8ce316eb82be2d
eduarda <e.qlourennco@gmail.com>
2025-08-08 15:27:20 -0300

    🔥feature: new pages: dashboard and new trade

--- cardswap/src/pages/CreateTrade.vue
+++ cardswap/src/pages/CreateTrade.vue
@@ -1,13 +1,15 @@
 <template>
-  <section class="page">
-    <h1>Criar Solicitação de Troca</h1>
-    <p>Selecione as cartas que deseja oferecer e receber.</p>
-    <!-- Depois vamos implementar o formulário -->
+  <section class="page max-w-6xl mx-auto px-4 py-12">
+    <div class="text-center mb-8">
+      <h1 class="text-2xl font-bold text-gray-900 mb-2">New Trade Request</h1>
+      <p>Select the cards you wish to offer and receive.</p>
+    </div>
+    <!-- form -->
   </section>
 </template>
 
 <script setup lang="ts">
-// Depois implementamos a lógica
+// lógica
 </script>
 
 <style scoped>
--- cardswap/src/pages/Dashboard.vue
+++ cardswap/src/pages/Dashboard.vue
@@ -1,12 +1,20 @@
 <template>
-  <section class="page">
-    <h1>Dashboard</h1>
-    <p>Bem-vindo ao seu painel, aqui você verá suas cartas e trocas.</p>
-    <router-link to="/trades/new">Criar nova troca</router-link>
+  <section class="page max-w-6xl mx-auto px-4 py-12">
+    <div class="text-center mb-8">
+      <h1 class="text-2xl font-bold text-gray-900 mb-2">Dashboard</h1>
+      <p>Welcome to your dashboard, here you will see your cards and trades.</p>
+    </div>
+
+    <div class="flex justify-center mb-6">
+      <NewTradeButton />
+    </div>
+    
+    
   </section>
 </template>
 
 <script setup lang="ts">
+  import NewTradeButton from "../components/NewTradeButton.vue";
 // Aqui vamos puxar dados do usuário depois
 </script>
 
